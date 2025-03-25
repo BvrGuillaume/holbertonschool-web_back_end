f0b0831b52493fb2faf87962ff45a1a3308c5b88
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-03-25 10:55:34 +0100

    all tasks

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueorFalse) {
+	const task = false;
+	const task2 = true;
+
+	if (trueorFalse) {
+		console.log(task, task2);
+		return [task, task2];
+	}
+
+	return [task, task2];
+}
---
+++ ES6_basic/README.md
@@ -0,0 +1,1 @@
+This is a read me for ES6 Basics
