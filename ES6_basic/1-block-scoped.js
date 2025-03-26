8237e1751dc166d404e4f62c88e0910ee8e2a064
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-03-26 12:24:34 +0100

    modify variables inside function

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
-export default function taskBlock(trueorFalse) {
-	const task = false;
-	const task2 = true;
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
 
-	if (trueorFalse) {
-		console.log(task, task2);
-		return [task, task2];
-	}
+    if (trueOrFalse) {
+        const task = true;
+        const task2 = false;
+    }
 
-	return [task, task2];
+    return [task, task2];
 }
