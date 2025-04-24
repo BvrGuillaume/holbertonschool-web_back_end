397239c3a08b90fb4a02dae9ce661bb244152a41
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-04-24 10:04:19 +0200

    task5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
-# 0-add.py
 from typing import List
+
 def sum_list(input_list: List[float]) -> float:
+    '''
+    Calcule la somme des éléments d'une liste de nombres flottants.
+
+    Args:
+        input_list (List[float]): Une liste de nombres flottants.
+
+    Returns:
+        float: La somme des nombres dans la liste.
+    '''
     return sum(input_list)
