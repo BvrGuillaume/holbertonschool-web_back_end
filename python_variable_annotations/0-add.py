dd8af11bbdc42b224609a4e775e1cd44b94df68d
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-04-24 10:20:57 +0200

    all tasks

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+"""
+function add with floats numbers
+ """
+
+
 def add(a: float, b: float) -> float:
+    """
+    adding function of the numbers type float
+    """
     return a + b
--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
+"""Module that provides a function to sum a list of floats."""
+
+
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    '''
-    Calcule la somme des éléments d'une liste de nombres flottants.
+    """Return the sum of a list of float numbers.
 
     Args:
-        input_list (List[float]): Une liste de nombres flottants.
+        input_list (List[float]): A list of float numbers.
 
     Returns:
-        float: La somme des nombres dans la liste.
-    '''
+        float: The total sum of the list.
+    """
     return sum(input_list)
