7b6d84ff6e768ec59cd3846348c9543574359743
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-04-28 11:18:52 +0200

    all tasks

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import asyncio
+import time
+from typing import List
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.perf_counter()
+    return end_time - start_time
+'''Mesure le temps d'exécution total de 4 exécutions de async_comprehension en parallèle et retourne le temps d'éxécution '''
