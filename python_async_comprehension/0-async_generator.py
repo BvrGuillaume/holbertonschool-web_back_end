5240523acd4679bac81d39df78d9fdc335a6bd74
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-04-28 13:38:53 +0200

    all tasks with args

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """Coroutine called async_generator that takes no args"""
+
+
 import asyncio
 import random
 import typing
 
+
 async def async_generator()-> typing.AsyncGenerator[float, None]:
     """The coroutune will yield a random float between 0 and 10, 10 times"""
+   
     for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Measure_runtime coroutine that will execute."""
+
 import asyncio
 import time
+
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
